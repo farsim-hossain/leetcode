af01d3c1146537c5e176196ba36bc6f8180f6618
farsim <farsimhossain@gmail.com>
2023-12-18 10:53:46 -0500

    new sql problem added

--- SQL/leetcode_sql.sql
+++ SQL/leetcode_sql.sql
@@ -1,6 +1,6 @@
 -- Write a solution to find the ids of products that are both low fat and recyclable.
 
-# Write your MySQL query statement below
+
 SELECT 
     product_id 
 FROM 
@@ -10,10 +10,22 @@ WHERE
 
 -- Find the names of the customer that are not referred by the customer with id = 2.
 
-# Write your MySQL query statement below
 SELECT 
     name
 FROM 
     Customer
 WHERE 
     referee_id != 2 OR referee_id IS NULL ;
+
+-- A country is big if:
+
+-- it has an area of at least three million (i.e., 3000000 km2), or
+-- it has a population of at least twenty-five million (i.e., 25000000).
+-- Write a solution to find the name, population, and area of the big countries.
+
+SELECT 
+    name, population, area 
+FROM 
+    World
+WHERE 
+    area >= 3000000 OR population >= 25000000 ;
